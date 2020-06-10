910857902a113f2857f4a71f9ab2ee81605f2215
takuma <s317062@ed.sus.ac.jp>
2020-06-10 12:49:39 +0900

    fix main.py
    discord.ext.commandsを使った設計に変更

--- src/main.py
+++ src/main.py
@@ -1,19 +1,59 @@
 import sys
 
 import discord
+from discord.ext import commands
 
 if __name__ == "__main__":
     print(f"discord.py version: {discord.__version__}")
     print("input bot token:", end = '')
     token = input()
 
-    client = discord.Client()
+    bot = commands.Bot(command_prefix = '$')
 
-    @client.event
+    @bot.command()
+    async def say(ctx: commands.Context, str: str):
+        '''botに発言させる 詳細は'$say help' '''
+        if str == "help":
+            embed = discord.Embed(title = "usage: $say [str]")
+            embed.add_field(name = "str", value = "strをbotが発言する")
+            await ctx.send(embed = embed)
+        else:
+            await ctx.send(str)
+
+    @say.error
+    async def say_error(ctx: commands.Context, ec: commands.CommandError):
+        if isinstance(ec, commands.MissingRequiredArgument):
+            await ctx.send("コマンド'$say'に引数が足りない\nTry '$say help' for more information")
+
+    @bot.command()
+    async def voice(ctx: commands.Context, option: str):
+        ''' botと音声チャンネル関連コマンド 詳細は'$voice help' '''
+        if option == "connect":
+            if ctx.author.voice is None:
+                await ctx.send("先にコマンド送信者がボイスチャンネルに入っている必要性があります")
+            else:
+                await ctx.author.voice.channel.connect()
+        elif option == "disconnect":
+            if ctx.guild.voice_client is None:
+                await ctx.send("Botがボイスチャンネルに入っていません")
+            else:
+                await ctx.guild.voice_client.disconnect()
+        elif option == "help":
+            embed = discord.Embed(title = "usage: $voice [cmd]")
+            embed.add_field(name = "connect", value = "botを音声チャンネルに入室させる", inline = False)
+            embed.add_field(name = "disconnect", value = "botを音声チャンネルから退出させる", inline = False)
+            await ctx.send(embed = embed)
+
+    @voice.error
+    async def voice_error(ctx: commands.Context, ec: commands.CommandError):
+        if isinstance(ec, commands.MissingRequiredArgument):
+            await ctx.send("コマンド'$voice'に引数が足りない\nTry '$voice help' for more information")
+
+    @bot.event
     async def on_ready():
-        print(f"We have logged in as {client.user}")
+        print(f"We have logged in as {bot.user}")
 
-        servers = client.guilds
+        servers = bot.guilds
         
         if servers is None:
             print("Bot is not on the server", file = sys.stderr)
@@ -25,23 +65,6 @@ if __name__ == "__main__":
             # channel.positionで一番上にあるチャンネルに送ることができるがラグでposition:0が存在しないことがありutils.getの戻り値がNoneになる可能性ありなので要検討
             # TODO: システムメッセージチャンネルに送りたいがdiscord APIに存在しない?
             main_text_channel = discord.utils.find(lambda c: c.type == discord.ChannelType.text, server.channels)
-            await main_text_channel.send(f"bot({client.user}) is online!")
+            await main_text_channel.send(f"bot({bot.user}) is online!")
 
-    # TODO: discord.ext.commandsに書き換える
-    @client.event
-    async def on_message(message):
-        if message.author != client.user:
-            if message.content == "$Hello":
-                await message.channel.send("Hello")
-            elif message.content == "$Voice":
-                if message.author.voice is None:
-                    await message.channel.send("先にコマンド送信者がボイスチャンネルに入っている必要性があります")
-                else:
-                    await message.author.voice.channel.connect()
-            elif message.content == "$Voice Dis":
-                if message.channel.guild.voice_client is None:
-                    await message.channel.send("Botがボイスチャンネルに入っていません")
-                else:
-                    await message.channel.guild.voice_client.disconnect()    
-
-    client.run(token)
+    bot.run(token)
